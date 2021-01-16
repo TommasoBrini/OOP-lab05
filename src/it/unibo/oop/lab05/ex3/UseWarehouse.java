a1f9cd8fd4342592357afcb3fccfc8ac0e0324d4
Tommaso <tommaso.brini@studio.unibo.it>
2021-01-16 17:23:35 +0100

    Solved ex03

--- src/it/unibo/oop/lab05/ex3/UseWarehouse.java
+++ src/it/unibo/oop/lab05/ex3/UseWarehouse.java
@@ -27,11 +27,11 @@ public final class UseWarehouse {
          * them to run the test.
          */
 
-        final Product p1 = null;
-        final Product p2 = null;
-        final Product p3 = null;
+        final Product p1 = new ProductImpl("banane", 1);
+        final Product p2 = new ProductImpl("pere", 3);
+        final Product p3 = new ProductImpl("mele", 2);
 
-        final Warehouse w = null;
+        final Warehouse w = new WarehouseImpl();
         w.addProduct(p3);
         w.addProduct(p1);
         w.addProduct(p2);
